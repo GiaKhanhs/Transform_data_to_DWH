21801940d96df0d4606424d00110e5a6eba0d1c1
Giakhanhs <giakhanh2091@gmail.com>
2024-06-12 16:53:59 +0700

    Update territory dimension table

--- models/mart/dim_territory.sql
+++ models/mart/dim_territory.sql
@@ -1,5 +1,11 @@
 SELECT 
-    *
-FROM {{ ref('stg_dim_territory') }}
-LEFT JOIN {{ ref('stg_dim_subterritory') }}
-USING (country_region_key)
+    stg_dim_territory.territory_key
+    , stg_dim_territory.territory_name
+    , stg_dim_territory.country_region_key
+    , stg_dim_subterritory.country_region_name
+    , stg_dim_territory.group_name
+
+FROM pj2_dbt_stg.stg_dim_territory 
+
+LEFT JOIN pj2_dbt_stg.stg_dim_subterritory
+ON stg_dim_territory.country_region_key = stg_dim_subterritory.country_region_key
